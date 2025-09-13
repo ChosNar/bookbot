2359c2b5b1dec0dc68264787f81d8aab2388333b
ChosNar <atmaneopuerto@gmail.com>
2025-09-13 16:42:44 -0600

    made changes added funct that takes the file returns it as a string than counts the number of words

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+def main():
+    txt = get_book_text("books/frankenstein.txt")
+#originally had txt_size under text_count function but the assignment required it to be a string 
+    word_num = text_count(txt)
+    print(f"{word_num} words found in the document")
 def get_book_text(file_path):
     with open(file_path) as file:
         return file.read()
-
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(text) 
+def text_count(txt):
+    words = txt.split()
+    return len(words)
 main()
+# Boots recommended adding a guard for main function
+#if __name__ == "__main__": main()
